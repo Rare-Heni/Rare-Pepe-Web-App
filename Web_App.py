a183bac484025dff488821956536fd0de4d028f2
Rare-Heni <105215119+Rare-Heni@users.noreply.github.com>
2022-07-18 22:48:55 +0200

    Title Indices Cumulative Returns

--- Web_App.py
+++ Web_App.py
@@ -1510,7 +1510,7 @@ else:
     if bitcoin_index == True:
 
         bi1, bi2 = st.columns((2, 3)) 
-        bi1.subheader("Benchmark Rare Pepe - Bitcoin")
+        bi1.subheader("Benchmark Rare Pepe - Bitcoin (Cumulative Returns)")
         i1, i2 = st.columns((2, 2))
 
         # Import Bitcoin data
@@ -1540,7 +1540,7 @@ else:
     if xcp_index == True:
 
         xcp1, xc2 = st.columns((2, 3)) 
-        xcp1.subheader("Benchmark Rare Pepe - XCP")
+        xcp1.subheader("Benchmark Rare Pepe - XCP (Cumulative Returns)")
         j1, j2 = st.columns((2, 2))
         
         # Import XCP data
@@ -1569,7 +1569,7 @@ else:
     if pepecash_index == True:
 
         pepe1, pepe2 = st.columns((2, 3)) 
-        pepe1.subheader("Benchmark Rare Pepe - Pepe Cash")
+        pepe1.subheader("Benchmark Rare Pepe - Pepe Cash (Cumulative Returns)")
         k1, k2 = st.columns((2, 2))
         
         # Import Pepe Cash data
@@ -1598,7 +1598,7 @@ else:
     if nasdaq_index == True:
 
         nas1, nas2 = st.columns((2, 3)) 
-        nas1.subheader("Benchmark Rare Pepe - NASDAQ")
+        nas1.subheader("Benchmark Rare Pepe - NASDAQ (Cumulative Returns)")
         l1, l2 = st.columns((2, 2))
 
         # Import NASDAQ data
