667c8b80ff210f9b31ca7c9ca96a371a381c9781
Rare-Heni <105215119+Rare-Heni@users.noreply.github.com>
2022-07-29 21:31:08 +0200

    Update Web_App.py

--- Web_App.py
+++ Web_App.py
@@ -1529,7 +1529,7 @@ else:
     
     # Missing selection of benchmark index
     if benchmark == True and bitcoin_index == False and xcp_index == False and pepecash_index == False and nasdaq_index == False:
-        i2.markdown("<h1 style='text-align: center; color: green;font-size:20px;'>Please select a benchmark index!</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center; color: green;font-size:20px;'>Please select a benchmark index!</h1>", unsafe_allow_html=True)
     
     # Selection for benchmark index
     if benchmark == True:
