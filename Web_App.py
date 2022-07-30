b5106a2039a130e53c75254f3f7983336eb02e82
Rare-Heni <105215119+Rare-Heni@users.noreply.github.com>
2022-07-30 15:41:55 +0200

    Update Web_App.py

--- Web_App.py
+++ Web_App.py
@@ -1693,7 +1693,7 @@ else:
             plt.plot('time','Price_Level',data = df, color = "green")
             with l1:
                 fig_test = mpld3.fig_to_html(fig_1)
-                components.html(fig_test, height=450, width=650)
+                components.html(fig_test, height=500, width=500)
 
             st.markdown("<hr/>", unsafe_allow_html=True)
 
