9e6da0f8527e710c5f6b635cb53403b2711dbbd5
Rare-Heni <105215119+Rare-Heni@users.noreply.github.com>
2022-07-30 15:39:03 +0200

    Update Web_App.py

--- Web_App.py
+++ Web_App.py
@@ -1694,6 +1694,7 @@ else:
             with l1:
                 fig_test = mpld3.fig_to_html(fig_1)
                 components.html(fig_test, height=850, width=850)
+                st.markdown("<hr/>", unsafe_allow_html=True)
 
             st.markdown("<hr/>", unsafe_allow_html=True)
 
